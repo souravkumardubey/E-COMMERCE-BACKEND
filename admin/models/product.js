2dcb83b60210d21fdec71eec8c89f77854438dcb
souravkumardubey <skdlp2000@gmail.com>
2023-06-29 13:54:15 +0530

    product schema added

---
+++ admin/models/product.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const productSchema = new mongoose.Schema({
+  productName: {
+    type: String,
+    required: true,
+    min: 1,
+    max: 255,
+  },
+  productCategory: {
+    type: String,
+    required: true,
+    min: 1,
+    max: 255,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  discountedPrice: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  inStock: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+const validateProduct = (user) => {
+  const schema = Joi.object({
+    productName: Joi.string().required().min(1),
+    productCategory: Joi.string().required().min(1),
+    price: Joi.number().integer().required().min(0),
+    discountedPrice: Joi.number().integer().required().min(0),
+    inStock: Joi.number().integer().required().min(1),
+    description: Joi.string().required(),
+  });
+  return schema.validate(user);
+};
+
+exports.Product = Product;
+exports.validateProduct = validateProduct;
---
+++ admin/routes/addProduct.js
@@ -0,0 +1,11 @@
+const Joi = require("joi");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+
+router.post("/add", (req, res) => {});
+
+module.exports = router;
