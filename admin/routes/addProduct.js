65c5262293bb047f21709bf2001c47ed11746c18
souravkumardubey <skdlp2000@gmail.com>
2023-06-30 21:17:26 +0530

    add product setup

--- admin/routes/addProduct.js
+++ admin/routes/addProduct.js
@@ -5,7 +5,12 @@ const _ = require("lodash");
 const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
+const { Product, validateProduct } = require("../models/product");
+const multer = require("multer");
 
-router.post("/add", (req, res) => {});
+router.post("/add", (req, res) => {
+  const { error, value } = validateProduct(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+});
 
 module.exports = router;
