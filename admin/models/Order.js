28f87e69a7ecc4bf80d84d96f2ee2e2290fe5290
souravkumardubey <skdlp2000@gmail.com>
2023-07-04 23:57:00 +0530

    designed orders schema

---
+++ admin/models/Order.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const orderSchema = new mongoose.Schema({
+  orders: {
+    type: [
+      {
+        productId: {
+          type: String,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        totalCost: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    required: true,
+  },
+  orderedDate: {
+    type: Date,
+    required: true,
+  },
+  orderStatus: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Order = mongoose.model("Order", productSchema);
+
+// const validateOrder = (user) => {
+//   const schema = Joi.object({});
+//   return schema.validate(user);
+// };
+
+exports.Order = Order;
+exports.validateOrder = validateOrder;
