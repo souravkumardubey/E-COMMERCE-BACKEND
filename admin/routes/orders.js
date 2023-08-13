2b09d3ca164916428dc35896b54e91eeed65d5e5
souravkumardubey <skdlp2000@gmail.com>
2023-08-13 20:59:39 +0530

    fixed importing packages and error response

--- admin/routes/orders.js
+++ admin/routes/orders.js
@@ -1,3 +1,4 @@
+const express = require("express");
 const Joi = require("joi");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
@@ -20,7 +21,8 @@ router.get("/", async (req, res) => {
 router.post("/new-order", async (req, res) => {
   try {
     const { error, value } = validateOrder(req.body);
-    if (error) return res.status(404).send(new Error(error.details[0].message));
+    if (error) return res.status(404).send(error.details[0].message);
+
     const order = new Order({
       ..._.pick(req.body, [
         "items",
@@ -33,7 +35,7 @@ router.post("/new-order", async (req, res) => {
     await order.save();
     res.status(200).send(order);
   } catch (error) {
-    return res.status(404).send(new Error(error.details[0].message));
+    return res.status(404).send(new Error("Something went wrong"));
   }
 });
 
@@ -47,12 +49,13 @@ router.get("/order/:id", async (req, res) => {
   }
 });
 
-router.put("/order/:id", async (req, res) => {
+router.put("/order-accepted/:id", async (req, res) => {
   try {
     const order = await Order.findByIdAndUpdate(req.params.id, {
       orderStatus: "Accepted",
     });
-    if (!order) return res.status(404).json({ message: "No product found." });
+    if (!order) return res.status(404).send(error.details[0].message);
+    await order.save();
     return res.status(200).send(order);
   } catch (error) {
     return res.status(404).json({ message: "No product found." });
