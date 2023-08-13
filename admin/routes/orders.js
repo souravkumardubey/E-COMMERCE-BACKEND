f3f6a07faaae02114c6fe4b0730a8c9d04bf719c
souravkumardubey <skdlp2000@gmail.com>
2023-08-13 18:32:00 +0530

    route to place new order and view order added

--- admin/routes/orders.js
+++ admin/routes/orders.js
@@ -5,7 +5,7 @@ const _ = require("lodash");
 const router = express.Router();
 const { Order, validateOrder } = require("../models/Order");
 
-router.post("/orders", async (req, res) => {
+router.get("/orders", async (req, res) => {
   try {
     const orders = await Order.find();
     if (!orders)
@@ -14,6 +14,36 @@ router.post("/orders", async (req, res) => {
     return res.status(200).send(orders);
   } catch (error) {
     return res.status(404).json({ message: "Something went wrong." });
+  }
+});
+
+router.post("/orders/new-order", async (req, res) => {
+  try {
+    const { error, value } = validateOrder(req.body);
+    if (error) return res.status(404).send(new Error(error.details[0].message));
+    const order = new Order({
+      ..._.pick(req.body, [
+        "items",
+        "orderedDate",
+        "address",
+        "totalPrice",
+        "orderStatus",
+      ]),
+    });
+    await order.save();
+    res.status(200).send(order);
+  } catch (error) {
+    return res.status(404).send(new Error(error.details[0].message));
+  }
+});
+
+router.get("/order/:id", async (req, res) => {
+  try {
+    const order = await Order.findById({ _id: req.params.id });
+    if (!order) return res.status(404).json({ message: "No order found." });
+    return res.status(200).send(order);
+  } catch (error) {
+    return res.status(404).json({ message: "No order found." });
   }
 });
 
