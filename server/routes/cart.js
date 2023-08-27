b76512c9530f4e4203a8c969b10a3892aca2c4e5
souravkumardubey <skdlp2000@gmail.com>
2023-08-27 20:44:59 +0530

    🚏 Add to cart route done

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -7,42 +7,42 @@ const router = express.Router();
 const { admin, user } = require("../middlewares/auth");
 const { Cart } = require("../models/Cart");
 
-const decodedToken = jwt.verify(
-  req.headers["auth-token"],
-  process.env.TOKEN_SECRET
-);
-
 router.post("/add/:id", async (req, res) => {
   try {
+    const decodedToken = jwt.verify(
+      req.headers["auth-token"],
+      process.env.TOKEN_SECRET
+    );
+
     const item = {
-      productId: req.body.productId,
+      productId: req.params.id,
       quantity: req.body.quantity,
       price: req.body.price,
     };
+
     const reqId = decodedToken.id;
     let cart = await Cart.findOne({ customerId: reqId });
+
     if (!cart) {
-      cart = new Order({
-        customerId: req.params.id,
-        items: [item],
-      });
-      await cart.save();
+      try {
+        cart = new Cart({
+          customerId: reqId,
+          items: [item],
+        });
+        await cart.save();
+      } catch (error) {
+        return res.status(404).send(error.message);
+      }
     } else {
       const updatedCart = await Cart.findByIdAndUpdate(cart._id, {
         $set: {
-          items: [...cart.items, prod],
+          items: [...cart.items, item],
         },
       });
     }
     return res.status(200).send("Product added to cart.");
   } catch (error) {
-    res.status(404).send(new Error("Something went wrong. Please try again!"));
-  }
-});
-router.get("/", async (req, res) => {
-  try {
-  } catch (error) {
-    return res.status(404).json({ message: "Something went wrong." });
+    res.status(404).send(error.message);
   }
 });
 
