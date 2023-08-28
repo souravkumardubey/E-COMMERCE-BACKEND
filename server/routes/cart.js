cd9b74ddcde5499508b800841314b7862fdd199f
souravkumardubey <skdlp2000@gmail.com>
2023-08-28 23:34:58 +0530

    🛒 All cart routes done

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -46,4 +46,80 @@ router.post("/add/:id", async (req, res) => {
   }
 });
 
+router.get("/", user, async (req, res) => {
+  try {
+    const decodedToken = jwt.verify(
+      req.headers["auth-token"],
+      process.env.TOKEN_SECRET
+    );
+    const reqId = decodedToken.id;
+    let cart = await Cart.findOne({ customerId: reqId });
+    if (!cart) return res.status(200).send({});
+    return res.status(200).send(cart);
+  } catch (error) {
+    return res.status(404).send(new Error(error.message));
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const decodedToken = jwt.verify(
+      req.headers["auth-token"],
+      process.env.TOKEN_SECRET
+    );
+
+    const reqId = decodedToken.id;
+    const itemIdToDelete = req.params.id;
+
+    let cart = await Cart.findOne({ customerId: reqId });
+
+    if (!cart) return res.status(404).send("Cart not found.");
+
+    const itemIndexToDelete = cart.items.findIndex(
+      (item) => item.productId === itemIdToDelete
+    );
+
+    if (itemIndexToDelete === -1)
+      return res.status(404).send("Item not found in cart.");
+
+    cart.items.splice(itemIndexToDelete, 1);
+    await cart.save();
+
+    return res.status(200).send("Item removed from cart.");
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const decodedToken = jwt.verify(
+      req.headers["auth-token"],
+      process.env.TOKEN_SECRET
+    );
+
+    const reqId = decodedToken.id;
+    const itemIdToUpdate = req.params.id;
+    const newQuantity = req.body.quantity;
+
+    if (typeof newQuantity !== "number" || newQuantity <= 0)
+      return res.status(400).send("Invalid quantity value.");
+
+    let cart = await Cart.findOne({ customerId: reqId });
+
+    if (!cart) return res.status(404).send("Cart not found.");
+
+    const itemToUpdate = cart.items.find((item) => item._id == itemIdToUpdate);
+
+    if (!itemToUpdate) return res.status(404).send("Item not found in cart.");
+
+    itemToUpdate.quantity = newQuantity;
+    await cart.save();
+
+    return res.status(200).send("Item quantity updated in cart.");
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
 module.exports = router;
